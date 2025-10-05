6bfa3bed4d9c99eaa04984823d10d3f599b4b05d
KAZI ABDUR RAKIB <rakib@pop-os.localdomain>
2025-10-05 15:46:42 +0800

    final

---
+++ init-security.groovy
@@ -0,0 +1,23 @@
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+println "--> Setting up admin user and disabling anonymous access"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("rakib", "rakib")   // change password
+hudsonRealm.createAccount("ci", "rakib")        // pipeline user
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new GlobalMatrixAuthorizationStrategy()
+strategy.add(Jenkins.ADMINISTER, "rakib")
+strategy.add(Jenkins.READ, "ci")
+strategy.add(Item.READ, "ci")
+strategy.add(Item.DISCOVER, "ci")
+strategy.add(Item.BUILD, "ci")
+strategy.add(Item.CANCEL, "ci")
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
+println "--> Security configuration complete"
